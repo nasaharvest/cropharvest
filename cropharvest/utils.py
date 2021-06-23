a222db0881ab062976004030adb75a7de4bfd0a9
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2021-06-23 08:56:11 -0700

    Save everything as h5 files instead of pickles

--- cropharvest/utils.py
+++ cropharvest/utils.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+import h5py
 
 import torch
 import numpy as np
 import random
+
+from typing import Dict
 
 
 DATAFOLDER_PATH = Path(__file__).parent.parent / "data"
@@ -12,3 +15,9 @@ def set_seed(seed: int = 42) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     random.seed(seed)
+
+
+def load_normalizing_dict(path_to_dict: Path) -> Dict[str, np.ndarray]:
+
+    hf = h5py.File(path_to_dict, "r")
+    return {"mean": hf.get("mean"), "std": hf.get("std")}
