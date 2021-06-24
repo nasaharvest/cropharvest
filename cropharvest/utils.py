84bff80099baaf95b4825a24776039fa4d49b9f6
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-06-23 18:20:33 -0700

    Initial downloading idea

--- cropharvest/utils.py
+++ cropharvest/utils.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from tqdm import tqdm
+from urllib.request import urlopen, Request
 
 import torch
 import numpy as np
@@ -12,3 +14,14 @@ def set_seed(seed: int = 42) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     random.seed(seed)
+
+
+def download_from_url(url: str, filename: str, chunk_size: int = 1024) -> None:
+    with open(filename, "wb") as fh:
+        with urlopen(Request(url)) as response:
+            with tqdm(total=response.length) as pbar:
+                for chunk in iter(lambda: response.read(chunk_size), ""):
+                    if not chunk:
+                        break
+                    pbar.update(chunk_size)
+                    fh.write(chunk)
