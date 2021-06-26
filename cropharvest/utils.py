8a9032f6e01628a559e5a700a67a85408dd07e92
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-06-25 17:41:06 -0700

    Check if torch is installed

--- cropharvest/utils.py
+++ cropharvest/utils.py
@@ -1,20 +1,23 @@
-from pathlib import Path
 import h5py
-
-import torch
 import numpy as np
 import random
-
+from pathlib import Path
 from typing import Dict
 
+try:
+    import torch
+    TORCH_INSTALLED = True
+except ImportError:
+    TORCH_INSTALLED = False
 
 DATAFOLDER_PATH = Path(__file__).parent.parent / "data"
 
 
 def set_seed(seed: int = 42) -> None:
     np.random.seed(seed)
-    torch.manual_seed(seed)
     random.seed(seed)
+    if TORCH_INSTALLED:
+        torch.manual_seed(seed)
 
 
 def load_normalizing_dict(path_to_dict: Path) -> Dict[str, np.ndarray]:
