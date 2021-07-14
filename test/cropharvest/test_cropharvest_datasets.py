7f23f77102d178f83a404aec3c07dbe390ec5bb7
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2021-07-14 06:39:15 -0700

    Add test for _get_positive_and_negative_indices

--- test/cropharvest/test_cropharvest_datasets.py
+++ test/cropharvest/test_cropharvest_datasets.py
@@ -19,3 +19,18 @@ def test_combination(monkeypatch, tmpdir) -> None:
                 assert cropharvest.y_vals[i] == 1
             else:
                 assert cropharvest.y_vals[i] == 0
+
+
+def test_get_positive_negative_indices(monkeypatch, tmpdir) -> None:
+    def __init__(self, root, task=None, download=False):
+        self.filepaths = ["a", "a", "b", "b", "b"]
+        self.y_vals = [1, 1, 0, 0, 0]
+
+    monkeypatch.setattr(CropHarvest, "__init__", __init__)
+
+    cropharvest = CropHarvest(root=tmpdir)
+
+    pos, neg = cropharvest._get_positive_and_negative_indices()
+
+    assert pos == [0, 1]
+    assert neg == [2, 3, 4]
