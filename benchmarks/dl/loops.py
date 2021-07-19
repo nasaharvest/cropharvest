d998c33447edc1db3d1acf5bed0e07a85fc50220
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2021-07-18 20:18:00 -1000

    More updates to deep learning benchmark

--- benchmarks/dl/loops.py
+++ benchmarks/dl/loops.py
@@ -8,13 +8,13 @@ from cropharvest.datasets import CropHarvest
 
 from .lstm import Classifier
 
-from typing import Optional
+from typing import Optional, List
 
 
 def train(
     classifier: Classifier,
     dataset: CropHarvest,
-    sample_size: Optional[float],
+    sample_size: Optional[int],
     num_grad_steps: int = 100,
     learning_rate: float = 0.001,
     k: int = 10,
@@ -24,7 +24,8 @@ def train(
     loss_fn = nn.BCELoss(reduction="mean")
 
     if sample_size is not None:
-        train_batch_total = dataset.sample_train(sample_size // 2)
+        train_batch_total = dataset.sample(sample_size // 2)
+        state: List[int] = []
 
     for i in tqdm(range(num_grad_steps)):
         if i != 0:
@@ -33,10 +34,12 @@ def train(
 
         if sample_size is not None:
             assert train_batch_total is not None
-            indices, state = sample_with_memory(train_batch_total, k * 2, state)
+            indices, state = sample_with_memory(
+                list(range(train_batch_total[0].shape[0])), k * 2, state
+            )
             train_x, train_y = train_batch_total[0][indices], train_batch_total[1][indices]
         else:
-            train_x, train_y = dataset.sample_train(k, deterministic=False)
+            train_x, train_y = dataset.sample(k, deterministic=False)
 
         preds = classifier(torch.from_numpy(train_x).float()).squeeze(dim=1)
         loss = loss_fn(preds, torch.from_numpy(train_y).float())
