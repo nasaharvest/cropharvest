7b01699918a3c9059ac312afe2ccaf0aec21ac83
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2021-07-18 20:04:12 -1000

    Random LSTM WIP

---
+++ benchmarks/deep_learning/__init__.py
@@ -0,0 +1,5 @@
+from .lstm import Classifier
+from .loops import train
+
+
+__all__ = ["Classifier", "train"]
--- benchmarks/deep_learning/loops.py
+++ benchmarks/deep_learning/loops.py
@@ -1,10 +1,12 @@
 from tqdm import tqdm
 from torch.optim import Adam
+import torch
+from torch import nn
 
+from cropharvest.utils import sample_with_memory
 from cropharvest.datasets import CropHarvest
 
 from .lstm import Classifier
-from ..config import PATIENCE
 
 from typing import Optional
 
@@ -15,9 +17,30 @@ def train(
     sample_size: Optional[float],
     num_grad_steps: int = 100,
     learning_rate: float = 0.001,
-) -> None:
+    k: int = 10,
+) -> Classifier:
 
     opt = Adam(classifier.parameters(), lr=learning_rate)
+    loss_fn = nn.BCELoss(reduction="mean")
+
+    if sample_size is not None:
+        train_batch_total = dataset.sample_train(sample_size // 2)
 
     for i in tqdm(range(num_grad_steps)):
-        classifier.train()
+        if i != 0:
+            classifier.train()
+            opt.zero_grad()
+
+        if sample_size is not None:
+            assert train_batch_total is not None
+            indices, state = sample_with_memory(train_batch_total, k * 2, state)
+            train_x, train_y = train_batch_total[0][indices], train_batch_total[1][indices]
+        else:
+            train_x, train_y = dataset.sample_train(k, deterministic=False)
+
+        preds = classifier(torch.from_numpy(train_x).float()).squeeze(dim=1)
+        loss = loss_fn(preds, torch.from_numpy(train_y).float())
+
+        loss.backward()
+        opt.step()
+    return classifier
