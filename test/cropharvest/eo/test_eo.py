5bbc157823159ee2dd6b7a3b1d999d13b3a11238
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-01-17 09:19:52 -0500

    tests for new code

--- test/cropharvest/eo/test_eo.py
+++ test/cropharvest/eo/test_eo.py
@@ -1,24 +1,29 @@
 import pandas as pd
 from datetime import date
+from unittest.mock import call, patch
 import pytest
 
+from cropharvest.columns import RequiredColumns
 from cropharvest.eo import EarthEngineExporter
+from cropharvest.eo.eo import get_cloud_tif_list
+
+
+def mock_bb_from_center(mid_lat, mid_lon, surrounding_metres):
+    return (mid_lon - 1, mid_lat - 1, mid_lon + 1, mid_lat + 1), None
 
 
 @pytest.mark.parametrize("with_identifier", (True, False))
 def test_labels_to_polygons_and_years(with_identifier, monkeypatch):
-    data = {
-        "lon": [-74.55285616553732],
-        "lat": [46.22024965230018],
-        "start_date": date(2020, 12, 15),
-        "end_date": date(2021, 12, 15),
-    }
+    labels = pd.DataFrame(
+        {
+            RequiredColumns.LON: [-74.55285616553732],
+            RequiredColumns.LAT: [46.22024965230018],
+            "start_date": date(2020, 12, 15),
+            "end_date": date(2021, 12, 15),
+        }
+    )
     if with_identifier:
-        data["export_identifier"] = ["hello_world"]
-    labels = pd.DataFrame(data)
-
-    def mock_bb_from_center(mid_lat, mid_lon, surrounding_metres):
-        return (mid_lon - 1, mid_lat - 1, mid_lon + 1, mid_lat + 1), None
+        labels["export_identifier"] = ["hello_world"]
 
     monkeypatch.setattr(EarthEngineExporter, "_bounding_box_from_centre", mock_bb_from_center)
 
@@ -36,3 +41,71 @@ def test_labels_to_polygons_and_years(with_identifier, monkeypatch):
             + "max_lon=-73.5529_dates=2020-12-15_2021-12-15_all"
         )
         assert identifier == expected_identifier
+
+
+@patch(f"cropharvest.eo.eo.storage")
+def test_get_cloud_tif_list(mock_storage):
+    mock_storage.Client().list_blobs("mock_bucket").return_value = []
+    tif_list = get_cloud_tif_list("mock_bucket")
+    assert tif_list == []
+
+
+@patch(f"cropharvest.eo.EarthEngineExporter._export_for_polygon")
+def test_export_for_labels(mock_export_for_polygon, monkeypatch):
+
+    start_date_str = "2019-04-22"
+    end_date_str = "2020-04-16"
+
+    labels = pd.DataFrame(
+        {
+            RequiredColumns.LON: [-74.55285616553732, -75.55285616553732],
+            RequiredColumns.LAT: [46.22024965230018, 47.22024965230018],
+            "end_date": [end_date_str, end_date_str],
+            "start_date": [start_date_str, start_date_str],
+        }
+    )
+    monkeypatch.setattr(EarthEngineExporter, "_bounding_box_from_centre", mock_bb_from_center)
+    EarthEngineExporter(check_gcp=False, check_ee=False, labels=labels).export_for_labels()
+
+    assert mock_export_for_polygon.call_count == 2
+
+    ending = f"dates={start_date_str}_{end_date_str}_all"
+    identifier_1 = f"min_lat=45.2202_min_lon=-75.5529_max_lat=47.2202_max_lon=-73.5529_{ending}"
+    identifier_2 = f"min_lat=46.2202_min_lon=-76.5529_max_lat=48.2202_max_lon=-74.5529_{ending}"
+    mock_export_for_polygon.assert_has_calls(
+        [
+            call(
+                checkpoint=True,
+                days_per_timestep=30,
+                end_date=end_date_str,
+                polygon=None,
+                polygon_identifier=identifier_1,
+                start_date=start_date_str,
+                test=False,
+            ),
+            call(
+                checkpoint=True,
+                days_per_timestep=30,
+                end_date=end_date_str,
+                polygon=None,
+                polygon_identifier=identifier_2,
+                start_date=start_date_str,
+                test=False,
+            ),
+        ],
+        any_order=True,
+    )
+
+
+def test_export():
+    labels = pd.DataFrame(
+        {
+            RequiredColumns.LON: [-74.55285616553732],
+            RequiredColumns.LAT: [46.22024965230018],
+            "start_date": date(2020, 4, 15),
+            "end_date": date(2021, 4, 15),
+        }
+    )
+    EarthEngineExporter(
+        check_gcp=True, check_ee=False, labels=labels, dest_bucket="cropharvest"
+    ).export_for_labels()
