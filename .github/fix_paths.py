044249126af61bd66db32a4d17ee2e4aa7ecd622
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2023-06-16 09:13:43 +0200

    Copy HDF5 DLLs into the h5py package for windows

---
+++ .github/fix_paths.py
@@ -0,0 +1,35 @@
+import distutils.sysconfig
+from glob import glob
+import os
+from os.path import join as pjoin, basename
+from shutil import copy
+from sys import platform
+
+def main():
+    """
+    Copy HDF5 DLLs into installed h5py package
+    """
+    # This is the function Tox also uses to locate site-packages (Apr 2019)
+    sitepackagesdir = distutils.sysconfig.get_python_lib(plat_specific=True)
+    print("site packages dir:", sitepackagesdir)
+
+    hdf5_path = os.environ.get("HDF5_DIR")
+    print("HDF5_DIR", hdf5_path)
+
+    # HDF5_DIR is not set when we're testing wheels; these should already have
+    # the necessary libraries bundled in.
+    if platform.startswith('win') and hdf5_path is not None:
+        for f in glob(pjoin(hdf5_path, 'lib/*.dll')):
+            copy(f, pjoin(sitepackagesdir, 'h5py', basename(f)))
+            print("Copied", f)
+
+        zlib_root = os.environ.get("ZLIB_ROOT")
+        if zlib_root:
+            f = pjoin(zlib_root, 'bin_release', 'zlib.dll')
+            copy(f, pjoin(sitepackagesdir, 'h5py', 'zlib.dll'))
+            print("Copied", f)
+
+        print("In installed h5py:", sorted(os.listdir(pjoin(sitepackagesdir, 'h5py'))))
+
+if __name__ == '__main__':
+    main()
