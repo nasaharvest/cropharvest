016301cb7f8f9585230bfc776bb2f1420899a8ac
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2023-06-16 09:14:40 +0200

    Run black against the fix_paths script

--- .github/fix_paths.py
+++ .github/fix_paths.py
@@ -5,6 +5,7 @@ from os.path import join as pjoin, basename
 from shutil import copy
 from sys import platform
 
+
 def main():
     """
     Copy HDF5 DLLs into installed h5py package
@@ -18,18 +19,19 @@ def main():
 
     # HDF5_DIR is not set when we're testing wheels; these should already have
     # the necessary libraries bundled in.
-    if platform.startswith('win') and hdf5_path is not None:
-        for f in glob(pjoin(hdf5_path, 'lib/*.dll')):
-            copy(f, pjoin(sitepackagesdir, 'h5py', basename(f)))
+    if platform.startswith("win") and hdf5_path is not None:
+        for f in glob(pjoin(hdf5_path, "lib/*.dll")):
+            copy(f, pjoin(sitepackagesdir, "h5py", basename(f)))
             print("Copied", f)
 
         zlib_root = os.environ.get("ZLIB_ROOT")
         if zlib_root:
-            f = pjoin(zlib_root, 'bin_release', 'zlib.dll')
-            copy(f, pjoin(sitepackagesdir, 'h5py', 'zlib.dll'))
+            f = pjoin(zlib_root, "bin_release", "zlib.dll")
+            copy(f, pjoin(sitepackagesdir, "h5py", "zlib.dll"))
             print("Copied", f)
 
-        print("In installed h5py:", sorted(os.listdir(pjoin(sitepackagesdir, 'h5py'))))
+        print("In installed h5py:", sorted(os.listdir(pjoin(sitepackagesdir, "h5py"))))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
