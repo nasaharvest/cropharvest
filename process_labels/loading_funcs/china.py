693a69267487092b6229118399d4bcd3c72491cb
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2023-10-18 12:49:22 -0400

    Type fixes

--- process_labels/loading_funcs/china.py
+++ process_labels/loading_funcs/china.py
@@ -106,9 +106,10 @@ def load_china() -> geopandas.GeoDataFrame:
         data=df, geometry=geopandas.points_from_xy(df.lon, df.lat), crs=LATLON_CRS
     )
     gdf = df.reset_index(drop=True)
-    gdf = gdf.rename(columns={"crop": RequiredColumns.IS_CROP})
+    gdf[RequiredColumns.IS_CROP] = df.apply(lambda x: 1 if x.crop == "Crop" else 0)
     gdf[RequiredColumns.INDEX] = df.index
     gdf[RequiredColumns.EXPORT_END_DATE] = df.apply(
         lambda x: datetime(x.year, EXPORT_END_MONTH, EXPORT_END_DAY), axis=1
     )
+    gdf[RequiredColumns.COLLECTION_DATE] = pd.to_datetime(df.collection_date)
     return gdf
